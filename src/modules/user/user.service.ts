5b81d73fc072b944ceb1cea828e5642e0e643138
Bogdan Rosynskyi <bogdan@darly.solutions>
2024-03-24 12:11:20 +0200

    - add fields for user table - add queries to database for user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,54 +1,81 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 
-import { CreateUserDto, UpdateUserDto, UserDto } from './dto';
+import { PaginatedResult, paginationHelper } from '@utils';
+
+import { UpdateUserDto, UserDto } from './dto';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  private users: Array<CreateUserDto & { id: number }> = [];
+  private readonly userSelect = {
+    id: true,
+    email: true,
+    firstName: true,
+    lastName: true,
+    createdAt: true,
+    updatedAt: true,
+    password: false,
+  };
+  constructor(private readonly prisma: PrismaService) {}
 
-  getUsers(): UserDto[] {
-    return this.users.map((user) => {
-      const userDto = new UserDto();
-      userDto.id = user.id;
-      userDto.firstName = user.firstName;
-      userDto.lastName = user.lastName;
-      userDto.email = user.email;
-      userDto.phoneNumber = user.phoneNumber;
-      return userDto;
+  getUsers(page: number): Promise<PaginatedResult<UserDto>> {
+    return paginationHelper<UserDto>(
+      (take, skip) => {
+        return this.prisma.$transaction([
+          this.prisma.user.count(),
+          this.prisma.user.findMany({
+            select: this.userSelect,
+            orderBy: {
+              createdAt: 'desc',
+            },
+            take,
+            skip,
+          }),
+        ]);
+      },
+      { page },
+    );
+  }
+
+  async getUser(where: { id?: number; email?: string }): Promise<UserDto> {
+    const user = await this.prisma.user.findFirst({
+      select: this.userSelect,
+      where,
     });
-  }
 
-  getUserById(id: number): UserDto {
-    const user = this.getUsers().find((user) => user.id === id);
-    if (user) {
-      return user;
+    if (!user) {
+      throw new NotFoundException();
     }
-    throw new NotFoundException();
+
+    return user;
   }
 
-  createUser(payload: CreateUserDto): UserDto {
-    const id = new Date().getTime();
-    this.users.push({ id, ...payload });
-    return this.getUserById(id);
+  // TODO: add controller for update user profile
+  async updateUserById(id: number, payload: UpdateUserDto): Promise<UserDto> {
+    try {
+      return await this.prisma.user.update({
+        select: this.userSelect,
+        where: { id },
+        data: payload,
+      });
+    } catch (e) {
+      throw new ForbiddenException();
+    }
   }
 
-  updateUserById(id: number, payload: UpdateUserDto): UserDto {
-    this.users = this.getUsers().map((user) => {
-      if (user.id === id) {
-        return {
-          ...user,
-          ...payload,
-        };
-      }
-      return user;
-    }) as Array<CreateUserDto & { id: number }>;
-
-    return this.getUserById(id);
-  }
-
-  delete(id: number): UserDto {
-    const deletedUser = this.getUserById(id);
-    this.users = this.users.filter((user) => user.id !== id);
-    return deletedUser;
+  // TODO: add controller for delete functionality  when user in system
+  async delete(id: number): Promise<UserDto> {
+    try {
+      return await this.prisma.user.delete({
+        select: this.userSelect,
+        where: { id },
+      });
+    } catch (e) {
+      throw new ForbiddenException();
+    }
   }
 }
